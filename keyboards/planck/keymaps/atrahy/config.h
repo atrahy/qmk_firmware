a2cd11641a56a3c1883711823130cd942886e649
Alexandre TRAHY <a.trahy@gmail.com>
2023-10-03 21:59:58 +0200

    feat(keymap/atrahy): reset previous keymap on new format

--- keyboards/planck/keymaps/atrahy/config.h
+++ keyboards/planck/keymaps/atrahy/config.h
@@ -3,10 +3,9 @@
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
+    // #define STARTUP_SONG SONG(ZELDA_PUZZLE)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND) \
                                 }
 #endif
 
@@ -33,4 +32,3 @@
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
-
